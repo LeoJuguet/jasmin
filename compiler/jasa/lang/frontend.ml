86f4ab1a66703d73ab6d5673d16ba6cc1bdca5b8
LeoJuguet <leo.juguet@hotmail.com>
2025-07-10 11:52:43 +0200

    add -I option

--- compiler/jasa/lang/frontend.ml
+++ compiler/jasa/lang/frontend.ml
@@ -11,9 +11,11 @@ module Arch = Ast.Arch
 
 let load_file name =
   let open Pretyping in
+  let idirs = !Glob_options.idirs in
+  let env = List.fold_left Pretyping.Env.add_from Pretyping.Env.empty idirs in
   try
     name
-    |> tt_file Arch.arch_info Env.empty None None
+    |> tt_file Arch.arch_info env None None
     |> fst |> Env.decls
     |> Compile.preprocess Arch.reg_size Arch.asmOp
   with
@@ -37,6 +39,16 @@ let () =
       doc = " select functions to check";
       spec = ArgExt.String (ArgExt.set_string_list_lifter opt_functions, ArgExt.empty);
       default = "all";
+    }
+
+let () =
+  register_language_option "Jasmin"
+    {
+      key = "-I";
+      category = "Jasmin";
+      doc = "include files";
+      spec = ArgExt.String (Jasmin.Glob_options.set_idirs, ArgExt.empty);
+      default = "";
     }
 
 (* ======================================================================  *)
@@ -67,13 +79,43 @@ module EntryDomainJasmin = struct
     let name = "jasmin.program"
   end)
 
-  let checks = []
+  type check += CHK_J_ENSURES
+
+  let () =
+    register_check (fun next fmt check ->
+      match check with
+      | CHK_J_ENSURES -> Format.fprintf fmt "Jasmin Ensures"
+      | _ -> next fmt check
+    )
+
+  type alarm_kind += A_J_ENSURES of expr
+
+  let () =
+    register_alarm {
+      check = (fun next -> function A_J_ENSURES _ -> CHK_J_ENSURES | a -> next a);
+      compare = (fun next a1 a2 ->
+        match (a1,a2) with
+        | A_J_ENSURES e1, A_J_ENSURES e2 -> compare_expr e1 e2
+        | _ -> next a1 a2
+      );
+      print = (
+        fun next fmt -> function
+          | A_J_ENSURES e ->
+              Format.fprintf fmt "'%a' doesn't holds"
+                pp_expr e
+          | a -> next fmt a
+      );
+      join = (fun next -> next);
+  }
+
+  let checks = [CHK_J_ENSURES]
 
   let init prog man flow =
     match prog.prog_kind with
     | Jasmin_Program jprog ->
         Debug.debug ~channel:name "init jasmin program\n";
-        set_jasmin_program jprog flow |> set_target_info (module Arch)
+        set_jasmin_program jprog flow
+        |> set_target_info (module Arch)
         |> Post.return
         |> OptionExt.return
     | _ -> None
@@ -96,12 +138,51 @@ module EntryDomainJasmin = struct
     in
 
     aux stub.stub_func_body []
+  
+  (* Get requires and transform them *)
+  let get_ensures stub man flow =
+    let open Stubs.Ast in
+    let rec aux sections man flow =
+      match sections with
+      | [] -> Cases.return () flow
+      | S_leaf (S_ensures ensures_with_range) :: q -> (
+          Debug.debug ~channel:name "is a leaf with requires";
+          let ensures = get_content ensures_with_range in
+          match get_content ensures with
+          | F_expr expr ->
+              assume expr
+                ~fthen:(fun flow ->
+                Flow.add_safe_check CHK_J_ENSURES (erange expr) flow
+                |> aux q man
+                )
+                ~felse:(fun flow ->
+                  let call_stack = Flow.get_callstack flow in
+                  let expr_origin = get_orig_expr expr in
+                  let alarm =
+                    mk_alarm (A_J_ENSURES expr_origin) call_stack expr.erange
+                  in
+                  Flow.raise_alarm alarm ~bottom:false ~warning:true man.lattice
+                    flow
+                  |> aux q man
+                ) man flow
+          | _ -> aux q man flow)
+      | _ :: q ->
+          Debug.debug ~channel:name "is not a leaf";
+          aux q man flow
+    in
+    aux stub.stub_func_body man flow
 
   let get_requires_in_assumes prog =
     let stub = prog.f_stub in
     let range = prog.f_loc in
     let requires = get_requires stub in
     requires
+  
+  let get_ensures_in_assumes prog =
+    let stub = prog.f_stub in
+    let range = prog.f_loc in
+    let ensures = get_ensures stub in
+    ensures
 
   let exec stmt man flow =
     match skind stmt with
@@ -116,6 +197,7 @@ module EntryDomainJasmin = struct
                (* add vars to values domains *)
                let range = srange body in
                let f = Flow.add T_cur flow in
+               let declare_args = declare_args prog.f_args in
                let add_vars =
                  List.map
                    (fun v ->
@@ -139,9 +221,16 @@ module EntryDomainJasmin = struct
                  locals_vars;
                let requires = get_requires_in_assumes prog in
                let new_block =
-                 Ast.mk_block (add_vars @ requires @ [ body ]) range
+                 Ast.mk_block (declare_args :: add_vars @ requires @ [ body ]) range
                in
-               man.exec new_block flow |> post_to_flow man |> Flow.remove T_cur)
+               (
+               man.exec new_block flow
+               >>% fun flow ->
+                 get_ensures_in_assumes prog man flow
+                )
+               |> post_to_flow man
+               |> Flow.remove T_cur
+             )
              prog.functions)
         |> Post.return |> OptionExt.return
     | _ -> None
